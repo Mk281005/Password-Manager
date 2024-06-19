0bea3d9f3ea06764bd6a5f834d7f4b1646d07487
Mk281005 <manoj281005@gmail.com>
2024-06-19 18:01:10 +0530

    Initial commit

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,12 +1,38 @@
 import React, { useState } from "react";
 import '../App.css';
+import { useEffect } from "react";
 
 function Manage() {
   const [Img, setImg] = useState("/src/assets/eye.svg");
+const [Type, setType] = useState("password")
+const [form, setform] = useState({site :"",nam : "",pass:""})
+const [passarr, setpassarr] = useState([])
+useEffect(() => {
+  let passwords = localStorage.getItem("password")
+  if(passwords){
+    setpassarr(JSON.parse(passwords))
+  }
+}, [])
+useEffect(() => {
+  console.log(passarr);
+}, [passarr]);
+
 
   const changeimg = () => {
     const newImg = Img === "/src/assets/eye.svg" ? "/src/assets/close.svg" : "/src/assets/eye.svg";
+    const newType =  Img === "/src/assets/eye.svg"?"text":"password";
+    setType(newType);
     setImg(newImg);
+  }
+  const savepass=() => {
+    setpassarr([...passarr,form])
+    localStorage.setItem("password",JSON.stringify([...passarr,form]))
+    console.log(passarr);
+    setForm({ site: "", nam: "", pass: "" });
+  }
+  
+  const handlechange =(e) => {
+    setform({...form,[e.target.name] : e.target.value});
   }
 
   return (
@@ -21,6 +47,9 @@ function Manage() {
         <div className="px-9 max-w-4xl w-full">
           <div className="mb-4">
             <input
+            name="site"
+            defaultValue={form.site}
+            onChange={handlechange}
               placeholder="Enter the website URL"
               type="text"
               className="w-full rounded-full focus:outline-none p-3 py-2 h-6"
@@ -28,6 +57,9 @@ function Manage() {
           </div>
           <div className="my-4 flex space-x-4">
             <input
+            name="nam"
+            defaultValue={form.nam}
+            onChange={handlechange}
               placeholder="Enter the Username"
               type="text"
               className="rounded-full w-4/6 p-3 py-2 focus:outline-none h-6"
@@ -35,13 +67,16 @@ function Manage() {
             <div className="flex relative">
               <img
                 src={Img}
-                onClick={changeimg}
+                onChange={changeimg}
                 className="absolute right-2 cursor-pointer"
                 alt="eye"
               /><form>
               <input
+              name="pass"
                 placeholder="Enter Password"
-                type="password"
+                onChange={handlechange}
+                defaultValue={form.pass}
+                type={Type}
                 className="rounded-full p-3 py-2 focus:outline-none h-6 w-64"
                  autoComplete="new-password"
               /></form>
@@ -51,7 +86,7 @@ function Manage() {
       </div>
       <div className="flex justify-center">
         
-          <button className="flex justify-center min-w-36 items-center rounded-full bg-slate-50 hover:bg-slate-200">
+          <button onClick={savepass} className="flex justify-center min-w-36 items-center rounded-full bg-slate-50 hover:bg-slate-200">
             <lord-icon
               src="https://cdn.lordicon.com/zrkkrrpl.json"
               trigger="hover"
